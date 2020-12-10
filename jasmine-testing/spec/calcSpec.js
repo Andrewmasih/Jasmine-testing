45c684a29526fab3633c71fdf6b47ac597c68cd4
Andrew Masih <armasih@outlook.com>
2020-12-10 20:16:36 +0000

    example of defensive programming

--- jasmine-testing/spec/calcSpec.js
+++ jasmine-testing/spec/calcSpec.js
@@ -6,7 +6,13 @@ describe("Calculator", function() {
         it("should return 26", function() {
             expect(addition(7,19)).toBe(26);
             
-        })
+        });
+        it("should return an error if we dont supply two numbers", function() {
+            expect(addition("hitchhikers", "guide")).toBe
+            ("error!");
+
+        });
+
     });
     
 });
